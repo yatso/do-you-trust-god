df446fd81a2c5aaebb360893c6c9953a34cbf202
yatso <yathso@gmail.com>
2017-10-04 15:58:55 -0700

    v1.1 button show/hide working with timeout

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,14 @@
 angular.module('app.controllers', [])
-  
-.controller('dOYOUTRUSTGODCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+    .controller('dOYOUTRUSTGODCtrl', ['$scope', '$stateParams', '$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $timeout) {
 
-
+            $scope.showmeFunction = function () {
+                $scope.showme = true;
+                $timeout(function () {
+                    $scope.showme = false;
+                }, 5000);
+            }
 }])
- 
